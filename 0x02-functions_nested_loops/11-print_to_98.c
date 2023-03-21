99f735f4fcf12e588262dbab08e84646ea2760d8
TheKharleeci <falodunmodupeola@gmail.com>
2023-03-21 15:38:33 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,8 +12,7 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%d, ", n);
-			n--;
+			printf("%d, ", n--);
 		}
 		printf("%d", n);
 	}
@@ -21,8 +20,7 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			printf("%d, ", n);
-			n++;
+			printf("%d, ", n++);
 		}
 		printf("%d", n)
 	}
