f90b4569b4638987abd5386d9ab6dd9a21de6a59
TheKharleeci <falodunmodupeola@gmail.com>
2023-03-21 12:21:46 -0700

    add print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet- Entry point
+ * Description: prints the alphabets in lower case
+ *
+ * returns- void
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,4 @@
 # FUNCTIONS AND NESTED LOOPS
+
+* 0-putchar.c: program that prints "_putchar"
+* 1-alphabet.c: function that prints the alphabet in lowercase
