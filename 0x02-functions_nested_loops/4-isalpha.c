c76586caff9099af1268eb354f620ce6cbe7ffb3
TheKharleeci <falodunmodupeola@gmail.com>
2023-03-21 13:31:32 -0700

    add isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha- checks if a number is ASCII lowercase or uppercase
+ * @c: the character to check
+ * Return: 1 if character is lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
