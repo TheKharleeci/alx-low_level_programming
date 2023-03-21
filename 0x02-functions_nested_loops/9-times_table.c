6c7e63710eb1f7b9ed111ccafa5cba124e143834
TheKharleeci <falodunmodupeola@gmail.com>
2023-03-21 16:02:39 -0700

    add times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the nine times table
+ * Returns: 0
+ */
+void times_table(void)
+{
+	int num, multiple, product;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar('0');
+
+		for (multiple = 0; multiple < 10; multiple++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = num * multiple;
+
+			if (product < 10)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
+
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
