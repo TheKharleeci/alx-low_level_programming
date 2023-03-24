623470775668bce8a22a722d82694b21516ae1e9
TheKharleeci <64368888+TheKharleeci@users.noreply.github.com>
2023-03-24 02:10:29 +0200

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (>= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
@@ -25,6 +25,5 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 
-	return largest;
-	return (0);
+	return (largest);
 }
