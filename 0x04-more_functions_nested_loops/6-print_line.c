f8aeaf5ebea73a230ffc37220591269b4b517685
TheKharleeci <falodunmodupeola@gmail.com>
2023-03-23 19:13:57 -0700

    add fn to print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line.
+ * @n: number of times the character _ should be printed
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		_putchar('_');
+		i++;
+	}
+
+	_putchar('\n');
+}
