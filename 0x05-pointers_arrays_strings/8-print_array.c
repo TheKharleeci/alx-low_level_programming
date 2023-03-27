40045f1395ae508ed5e8e0dd107ce9c7fca080a8
TheKharleeci <falodunmodupeola@gmail.com>
2023-03-27 14:45:08 -0700

    update 8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -22,8 +22,10 @@ void print_array(int *a, int n)
 	while (i < n && i < arr_length)
 	{
 		if (i == (n - 1) || i == (arr_length - 1))
+		{
 			printf("%d", a[i]);
 			break;
+		}
 		printf("%d, ", a[i]);
 		i++;
 	}
