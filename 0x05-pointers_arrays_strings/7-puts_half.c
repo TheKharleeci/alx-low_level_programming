8da0db1308310c83cf0a257f05ec519887c8b65f
TheKharleeci <64368888+TheKharleeci@users.noreply.github.com>
2023-03-27 23:43:04 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,13 +12,13 @@ void puts_half(char *str)
 	int strlen = 0;
 	int start_point, i;
 
-	while (str[strlen] != '\0')
+	while (str[strlen])
 	{
 		strlen++;
 	}
 
 	if (strlen % 2 != 0)
-		start_point = (strlen - 1) / 2;
+		start_point = (strlen + 1) / 2;
 	else
 		start_point = strlen / 2;
 
