4722e2e7ba281212889be96c4cd3e563670c47e7
TheKharleeci <falodunmodupeola@gmail.com>
2023-03-30 17:20:21 -0700

    use minimum length

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int first_str_len = 0;
 	int second_str_len = 0;
-	int max_len = first_str_len;
+	int min_len;
 
 	while (s1[first_str_len])
 	{
@@ -23,9 +23,11 @@ int _strcmp(char *s1, char *s2)
 	{
 		second_str_len++;
 	}
-	if (second_str_len - first_str_len > 0)
-		max_len = second_str_len;
-	for (i = 0; i < max_len; i++)
+	if (second_str_len <= first_str_len)
+		min_len = second_str_len;
+	else
+		min_len = first_str_len;
+	for (i = 0; i < min_len; i++)
 	{
 		if (s1[i] != s2[i])
 		{
