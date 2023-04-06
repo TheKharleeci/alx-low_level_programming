ae014aa826d485813bc1e8880bdf064c29eec7c5
TheKharleeci <falodunmodupeola@gmail.com>
2023-04-05 18:38:22 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * find_sqrt - evaluates the square root of a number
+ * @n: the number to evaluate
+ * @start: the start point
+ * @end: the end point
+ * Return: int
+ */
+int find_sqrt(int n, int start, int end)
+{
+	int mid;
+	int end = n;
+
+	if (start <= end)
+	{
+		mid = (start + end) / 2;
+		if (mid * mid == n)
+		{
+			return (mid);
+		}
+		if (mid * mid < n)
+		{
+			return (find_sqrt(n, mid + 1, end));
+		}
+		else
+		{
+			return (find_sqrt(n, start, mid - 1));
+		}
+	}
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: the number
+ *
+ * Return: int (the square root of n or -1)
+ */
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+	return (find_sqrt(int n, 0, n));
+}
