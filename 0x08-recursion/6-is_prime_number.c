a8ef6bba1fd939d9d0ea82a4bf2803395f3a9ed9
TheKharleeci <falodunmodupeola@gmail.com>
2023-04-05 17:35:01 -0700

    update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,12 +14,23 @@ int is_prime_number(int n)
 	i = 2;
 
 	if (n == 0 || n == 1)
+	{
 		return (0);
+	}
+	if (i > n / 2)
+	{
+		return (1);
+	}
 	if (i <= n / 2)
 	{
 		if (n % i == 0)
+		{
 			return (0);
-		i++;
-		return (is_prime_number(n));
+		}
+		else
+		{
+			i++;
+			return (is_prime_number(n));
+		}
 	}
 }
