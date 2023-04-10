aaf28f94a17eed4b6affde393722d980d0652c6b
TheKharleeci <falodunmodupeola@gmail.com>
2023-04-10 15:06:45 -0700

    print arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the arguments
+ * @argc: the count of the arguments
+ * @argv: the array of the arguments
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,6 @@
 # argc, argv
 * 0-whatsmyname - prints the program name
 * 1-args - prints the number of arguments passed into it
+* 2-args - prints all the arguments it receives
+* 3-mul - prints the multiplication of two numbers
+* 4-add - adds positive numbers
