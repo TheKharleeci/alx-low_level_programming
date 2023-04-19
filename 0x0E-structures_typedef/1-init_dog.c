454373b8732a451313af35f73b6130d24fc7326c
TheKharleeci <falodunmodupeola@gmail.com>
2023-04-18 17:07:56 -0700

    account for ull

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,7 @@
 #include "dog.h"
 
 /**
- * init_dog - initializes a variable of typr struct
+ * init_dog - initializes a variable of type struct
  * @d: the dog struct
  * @name: name of the dog
  * @age: dog age
@@ -11,7 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,2 @@
 # C - Structures, typedef
+* 1-init_dog - initializes a dog of typr struct
